82803454d2b4a323f91bd20838aa21900af0a01f
poojapdarade <poojap.darade@gmail.com>
2024-06-04 23:11:48 +0100

    slice function error

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 
 function Square({ value, onSquareClick }) {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -9,11 +10,9 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board(xIsNext, squares, onPlay) {
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -22,9 +21,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -55,6 +52,33 @@ export default function Board() {
     </>
   );
 }
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState(Array(9).fill(null));
+  const currentSquares = history[history.length - 1];
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  return (
+    <>
+      <div className="game">
+        <div className="game-board">
+          <Board
+            xIsNext={xIsNext}
+            squares={currentSquares}
+            onPlay={handlePlay}
+          />
+        </div>
+        <div className="game-info">
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    </>
+  );
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
